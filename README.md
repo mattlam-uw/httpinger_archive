2b60c98b849c2805aee69953c5a0f436b5953e7a
Matt <mattlam@uw.edu>
2015-11-20 12:14:02 -0800

    Updated README. Stories 1, 2, and 3 now working correctly

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # HTTPinger
 # Backlog
-| ID  | Pri | Story |
-| --- |:---:| ----- |
-| 1 | 1 | As a user, I want to be able to periodically ping a specified URL and have all response codes logged to a specified file. |
-| 2 | 1 | As a user, I want to be able to specify more than one URL to periodically ping. |
-| 3 | 1 | As a user, I want to be able to specify a constant value to represent how often the ping rounds occur. |
+| ID  | Story |
+| --- | ----- |
+| ~~1~~ | ~~As a user, I want to be able to periodically ping a specified URL and have all response codes logged to a specified file.~~ |
+| ~~2~~ | ~~As a user, I want to be able to specify more than one URL to periodically ping.~~ |
+| ~~3~~ | ~~As a user, I want to be able to specify a constant value to represent how often the ping rounds occur.~~ |
+| 4 | As a user, I want ping results to be written to a log file (not just output to stdout). |
+| 5 | As a user, I want to be able to see the response page data returned for status code 4xx and 5xx responses. |
--- server.js
+++ server.js
@@ -1,34 +1,42 @@
+// Node.js Module Dependencies
 var http = require('http');
 
-// Constant representing ping frequency in seconds
+// Constant representing ping frequency in seconds. This may later be put in a
+// config file.
 const PING_FREQ = 10;
 
-// Define object to contain URLs we want to ping
-var urls = [ 
+// Define set of urls to ping as an array of objects. This will later be put in
+// a separate file
+var urls = [
     { name: 'Research home page', host: 'www.washington.edu', path: '/research/' },
-    { name: 'Limited Submissions page', host: 'www.washington.edu', path: '/research/funding/limited-submissions/' },
+    { name: 'Limited Submissions page', host: 'www.washington.edu', path: '/research/funding/limiterd-submissions/' },
     { name: 'Funding Opportunities page', host: 'www.washington.edu', path: '/research/funding/opportunities/' },
     { name: 'Stats and Rankings page', host: 'www.washington.edu', path: '/research/spotlight/ranking/' }
 ];
 
 // Function to run through and ping all defined urls
 function pingUrls(arrUrls) {
-    // Output the time for this batch of ping requests
+    // Output the time for this batch of ping requests and a divider line to
+    // separate the batches
     var currentTime = getTime();
+    console.log('================');
     console.log(currentTime);
+
+    // Iterate through the array of objects containing the URLs to ping and
+    // send a request for each URL
     for (var i = 0; i < arrUrls.length; i++) {
-        // generate options
+        // Generate request options
         var options = generateOptions(arrUrls[i].host, arrUrls[i].path);
-        // generate callback
+        // Generate request callback
         var fullUrl = arrUrls[i].host + arrUrls[i].path;
         var callback = generateCallback(arrUrls[i].name, fullUrl)
-        // ping the url
+        // Send the request
         var req = http.request(options, callback);
         req.end();
     }
 }
-   
-// Generates options to be used for http.request
+
+// Function to generate options to be used for http.request
 function generateOptions(host, path) {
     return {
         host: host,
@@ -36,36 +44,52 @@ function generateOptions(host, path) {
     };
 }
 
-// Generates a callback to be used for http.request
+// Function to generate a callback to be used for http.request
 function generateCallback(urlName, fullUrl) {
     return function(res) {
-        // Log the response header info
-        console.log('Page Name:', urlName);
-        console.log('URL:', fullUrl);
-	// console.log('HTTP headers:', res.headers);
-        console.log('HTTP status code:', res.statusCode);
-        console.log('HTTP status message:', res.statusMessage);
+        // Output the response body (web page code)
+        var pageData = '';
+        res.on('data', function(data) {
+            // In case of status code 200, do nothing. Because of the way
+            // streaming works in node.js, you must listen for and consume
+            // that response data in order for the response 'end' event to
+            // be fired. See http://stackoverflow.com/questions/23817180/
+
+            // When status code is not 200, then capture the response payload
+            if (res.statusCode !== 200) {
+                pageData += data;
+            }
+        });
         // Indicate end of log
         res.on('end', function() {
-            console.log('*** fertig! ***');
+            // Log the response header info
+            console.log('----------------');
+            console.log('Page Name:', urlName);
+            console.log('URL:', fullUrl);
+            // console.log('HTTP headers:', res.headers);
+            console.log('HTTP status code:', res.statusCode);
+            if (pageData) console.log(pageData.toString());
         });
-        console.log('----------------');
-    }
+    };
 }
 
-// Gets current time and returns as a string
+// Function to return the current date and time as a string
 function getTime() {
     var currentDate = new Date();
-    var dateTime = currentDate.getDate() + "/"
-                 + currentDate.getHours() + ":"
-                 + currentDate.getMinutes() + ":"
-                 + currentDate.getSeconds();
+    var dateTime = (currentDate.getMonth() + 1) + "/"
+        + currentDate.getDate() + " - "
+        + currentDate.getHours() + ":"
+        + currentDate.getMinutes() + ":"
+        + currentDate.getSeconds();
     return dateTime;
 }
 
+// Wrapper function around the call to send requests for the URLs. This is
+// needed for use with setInterval as that method only allows a reference
+// to a function rather than a call to the function itself.
 function ping() {
     pingUrls(urls);
 }
 
-// Go ahead and ping away . . .
+// Send a batch of requests every [PING_FREQ] seconds
 var pingInterval = setInterval(ping, (PING_FREQ * 1000));
