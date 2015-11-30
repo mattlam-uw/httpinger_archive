02f0ec302ea3641d2c494090f1bc2d04b2dfcf53
Matt <mattlam@uw.edu>
2015-11-30 10:15:40 -0800

    Remove erroneously added .swp files and improve code commenting in all files

--- modules/httping.js
+++ modules/httping.js
@@ -1,22 +1,36 @@
+/**
+ * This module contains all methods needed to use the given URL data to send
+ * requests to the URLs. The only method currently exposed for client code
+ * use is pingUrls(). All other methods are used within this module.
+ *
+ * The initial request to a URL is always just a request for the response
+ * headers (method = 'HEAD'). If the response for any request results in a
+ * status code of 400 or more, then a full-page (method = 'GET') follow-up
+ * request is sent to the URL in order to get any valuable error message
+ * information that might be included in the page response.
+ */
+
 // Node.js Module Dependencies
 var http = require('http'); // Used to make HTTP requests
 var fs = require('fs');     // Used for reading and writing to local system files
 
 // Define constants. These may later be placed in a config file.
 const LOG_FILE_PATH = './logs/';     // Path to log files
-const LOG_FILE_NAME = 'logfile.txt'; // File name for standard log file
+const LOG_FILE_NAME = 'header_request_log.txt'; // File name for standard log file
 
 // Function to run through and ping all defined urls
 exports.pingUrls = function(arrUrls) {
     // Don't do anything if no parameter values provided
     if (!arrUrls) {
+        console.log('Error - no URL data provided');
         return;
     }
     
-    // Notify via STDOUT that a ping round is being executed and time of ping round
-    var currentTimeReadable = getTime(true);
-    var currentTimeCompact = getTime(false);
+    // Retrieve readable and compact forms of timestamp for this round of requests
+    var currentTimeReadable = getTime(false);
+    var currentTimeCompact = getTime(true);
 
+    // Notify STDOUT that a request round has been executed
     console.log(currentTimeReadable + ' - request round executed.');
 
     // Iterate through the array of objects containing the URLs to ping and
@@ -63,28 +77,25 @@ function generateCallback(urlName, urlHost, urlPath, method,
         // Upon response completion, kick off a follow-up request if needed and
         // log the response info
         res.on('end', function() {
-            // If the request method is HEAD, then do a follow-up GET request
-            // if the status code was >= 400. In all cases, log the output from
-            // the response.
+            // If the request method is HEAD, log the output from the response.
+            // If the status code was >= 400 as well, then do a follow-up GET
+            // request to retrieve the full page data.
             if (method == 'HEAD') {
                 
-                // Follow-up request in event of status code >= 400
+                // If status code >= 400, follow-up with a GET request.
                 if (res.statusCode >= 400) {
-                    // In cases where it is a HEAD request, send to follow-up
-                    // full request to get the full page
+                    // Set up for the follow-up request
                     var fullReqOptions = generateOptions(urlHost, urlPath, 'GET');
                     var fullReqCallback = generateCallback(urlName, urlHost,
                             urlPath, 'GET', readableTime, compactTime);
-                    var fullReq = http.request(fullReqOptions, fullReqCallback);
-                    fullReq.end();
+                    // Execute the follow-up request
+                    http.request(fullReqOptions, fullReqCallback).end();
                 }
 
-                // Log request results
+                // Log the 'HEAD' request results
                 logOutput += readableTime + '\n';
-                // Log the response header info
                 logOutput += 'Page Name: ' + urlName + '\n';
                 logOutput += 'URL: ' + urlHost + urlPath + '\n';
-                // logOutput += 'HTTP headers: ' + res.headers + '\n';
                 logOutput += 'HTTP status code: ' + res.statusCode + '\n';
                 if (pageData) logOutput += pageData.toString() + '\n';
                 logOutput += '================================\n';
@@ -97,7 +108,9 @@ function generateCallback(urlName, urlHost, urlPath, method,
                 });
             
             // If the request method is GET, this was a follow-up request for 
-            // a pull page. Log this in a separate file.
+            // a full page. Log this in a separate file. Each follow-up request
+            // gets its own file. The name of the file will be:
+            // "err-[timestamp]-[name for URL].html
             } else if (method == 'GET') {
                 logFilePath = LOG_FILE_PATH + 'err-' + compactTime + '-'
                     + noSpaceName + '.html';
@@ -111,12 +124,12 @@ function generateCallback(urlName, urlHost, urlPath, method,
 }
 
 // Function to return the current date and time as a string. Passing true for
-// the includeSpaces parameter provides a more readable version suitable for
-// readable output. Passing false provides a version more suitable for file names.
-function getTime(includeSpaces) {
+// the 'compact' parameter provides a compact no-space version suitable for file
+// names. Passing false provides an easier to read version.
+function getTime(compact) {
     var currentDate = new Date();
     var currentMonth = currentDate.getMonth() + 1;
-    if (includeSpaces) {
+    if (!compact) {
         return '' + currentDate.getFullYear()
             + currentMonth
             + currentDate.getDate() + " - "
--- server.js
+++ server.js
@@ -1,24 +1,45 @@
+/**
+ * Main application file
+ *
+ * Node.js Module Dependencies
+ *  + fs
+ *
+ * Internal Module Dependencies
+ *  + ./modules/httping.js
+ *  + ./modules/urlsIO.js
+ *
+ */
+
 // Node.js Module Dependencies
 var fs = require('fs');     // Used for reading and writing to local system files
 
 // Require local modules
-// Module containing all functions used for creating http requests
 var httping = require('./modules/httping.js');
 var urlsIO = require('./modules/urlsIO.js');
-
 
 // Define constants. These may later be placed in a config file.
 const PING_FREQ = 5;  // Request round frequency in seconds
 
-
-// Retrieve all URL data needed for making requests
+// Define the urls variable outside of getUrls function so that it can be referenced
+// by pingUrlHelper()
 var urls;
-urlsIO.getUrls(function(urlData) {
+
+// Callback function to be passed with call to getUrls(). This function will
+// assign the url data retrieved by getUrls() to the urls variable and then
+// begin sending time-interval-spaced requests to the urls. We need to kick off
+// the requests from within the callback in order to ensure that the url data
+// has all been retrieved via asynchronous calls to retrieve the data.
+var callback = function(urlData) {
+    // Assign the retrived url data to the urls variable
     urls = urlData;
 
-    // Send a round of requests every [PING_FREQ] seconds
+    // Kick off time-interval-spaced requests every [PING_FREQ] seconds
     var pingInterval = setInterval(pingUrlHelper, (PING_FREQ * 1000));
-});
+}
+
+// Call getUrls() with the above callback in order to get the URLs and kick
+// off the requests
+urlsIO.getUrls(callback);
 
 // Because setInterval accepts only a reference to function, not function call
 // itself, a helper function is needed to wrap the actual function call
